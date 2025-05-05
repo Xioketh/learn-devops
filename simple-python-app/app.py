1ae9c14b1419a155e127aff3f69b794dea50fe30
Kethaka Janadithya Ranasinghe <119161938+Xioketh@users.noreply.github.com>
2025-05-06 00:14:33 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -7,5 +7,4 @@ def hello():
     return 'Hello, Kethaka WHats up!'
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host='0.0.0.0', port=5000)  # Bind to all IPs so it's accessible from outside
