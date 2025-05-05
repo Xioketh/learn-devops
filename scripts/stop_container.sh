c073031f99febe7104aecee122dfdcb2a0f70798
Kethaka Janadithya Ranasinghe <xiojanadithya@gmail.com>
2025-05-06 00:29:58 +0530

    ui changes

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 set -e
-echo "Hii"
+
+# Get the container ID of the first running container
+containerid=$(sudo docker ps -q | head -n 1)
+
+# Check if a container ID was found
+if [ -n "$containerid" ]; then
+    echo "Stopping and removing container ID: $containerid"
+    sudo docker rm -f "$containerid"
+else
+    echo "No running containers found."
+fi
--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Kethaka WHats up!'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Bind to all IPs so it's accessible from outside
